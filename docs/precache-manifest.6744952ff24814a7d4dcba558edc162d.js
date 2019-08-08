86e2ac21fa4c5f5d9baf70feb84fc991b44e2f2c
mledezma <marcoledezmacordero09@gmail.com>
2019-08-08 09:00:59 -0600

    Initial commit

---
+++ docs/precache-manifest.6744952ff24814a7d4dcba558edc162d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e2d55474f83ab343ca64cac0076232",
+    "url": "/index.html"
+  },
+  {
+    "revision": "206eef0457439d02ee4e",
+    "url": "/static/css/main.c5c607c6.chunk.css"
+  },
+  {
+    "revision": "cd01c1454facd776d32a",
+    "url": "/static/js/2.2ddfe51a.chunk.js"
+  },
+  {
+    "revision": "206eef0457439d02ee4e",
+    "url": "/static/js/main.69d62ff4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
